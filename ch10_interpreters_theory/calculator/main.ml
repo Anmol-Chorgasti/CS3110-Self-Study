2b4732af41cb601a7f6d9aaeaa21112499457d75
Anmol Chorgasti <anmolchorgasti@gmail.com>
2026-06-22 15:21:13 +0200

    Skeleton for basic calculator project

---
+++ ch10_interpreters_theory/calculator/main.ml
@@ -0,0 +1,3 @@
+
+
+let interp _ = ""
---
+++ ch10_interpreters_theory/calculator/test.ml
@@ -0,0 +1,13 @@
+open OUnit2
+open Ast
+open Main
+
+(**[make_i n i s] makes an Ounit test named [n] that expects [s]
+   to evaluate to [i]. *)
+let make_i n i s =
+  n >:: (fun _ -> assert_equal (string_of_int i) (interp s))
+
+let tests = [
+  make_i "int" 22 "22"
+]
+
